c001608d080db700940f5ba05b48fc76aff74c4f
Kevin Britt <kbritt510@gmail.com>
2020-08-05 03:01:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,8 @@
 class Person
-  
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(name)
+    @name = name
   end
   
   def name
-    puts "#{@first_name} #{@last_name}"
+    @name
   end
-  
-end
